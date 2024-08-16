f15d2a33a4e1cf5460ed7eafdaa826a04485cca8
Kaik Xavier Marques de Oliveira <kaikmarques.ti@hotmail.com>
2024-08-16 09:44:55 -0300

    Add_Tela

--- BuscadorCepWebService/src/cep/Cep.java
+++ BuscadorCepWebService/src/cep/Cep.java
@@ -5,11 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class Cep extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -31,12 +40,67 @@ public class Cep extends JFrame {
 	 * Create the frame.
 	 */
 	public Cep() {
+		setTitle("Buscar CEP");
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Cep.class.getResource("/img/home.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("CEP");
+		lblNewLabel.setBounds(10, 25, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(60, 22, 97, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Endereço");
+		lblNewLabel_1.setBounds(10, 58, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(70, 55, 206, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Bairro");
+		lblNewLabel_2.setBounds(10, 94, 46, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(60, 91, 216, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Cidade");
+		lblNewLabel_3.setBounds(10, 134, 46, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(60, 131, 97, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("UF");
+		lblNewLabel_4.setBounds(195, 134, 24, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(216, 130, 60, 22);
+		contentPane.add(comboBox);
+		
+		JButton btnNewButton = new JButton("Limpar");
+		btnNewButton.setBounds(0, 196, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Buscar");
+		btnNewButton_1.setBounds(187, 21, 89, 23);
+		contentPane.add(btnNewButton_1);
 	}
-
 }
